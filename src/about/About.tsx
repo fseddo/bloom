f168e003104b171b3fc331780163846f6c682381
Francesco <francesco.seddo@gmail.com>
2024-02-04 18:33:32 -0500

    added aboutt page

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,3 +1,58 @@
 export const About = () => {
-  return <div>About Us</div>;
+  return (
+    <div className="flex flex-col items-center gap-10 p-10 py-16 px-72">
+      <div className="max-w-[900px]">
+        <img src="./about2.jpg" />
+      </div>
+      <div className="flex flex-col gap-8">
+        <div className="text-4xl font-bold">
+          Hey there! We are Bloom Flower Boutique.
+        </div>
+        <div className="text-xl font-bold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut morbi
+          tincidunt augue interdum velit. Sapien faucibus et molestie ac feugiat
+          sed lectus. Commodo nulla facilisi nullam vehicula ipsum a. Tempor nec
+          feugiat nisl pretium fusce id velit ut. Nisi quis eleifend quam
+          adipiscing vitae. Ultricies mi quis hendrerit dolor magna eget est
+          lorem. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Ac
+          tincidunt vitae semper quis lectus. Amet tellus cras adipiscing enim
+          eu turpis egestas. Dictumst vestibulum rhoncus est pellentesque elit
+          ullamcorper dignissim cras tincidunt. Aliquam faucibus purus in massa.
+        </div>
+
+        <div className="max-w-[900px] self-center">
+          <img src="./about3.jpg" />
+        </div>
+
+        <div className="text-xl font-bold">
+          Suspendisse faucibus interdum posuere lorem. Pretium aenean pharetra
+          magna ac placerat. Sit amet justo donec enim. Aliquam ut porttitor leo
+          a diam sollicitudin. Nullam vehicula ipsum a arcu cursus vitae. In
+          ornare quam viverra orci. Sed egestas egestas fringilla phasellus.
+          Eleifend quam adipiscing vitae proin. Facilisis magna etiam tempor
+          orci eu. Scelerisque viverra mauris in aliquam sem fringilla. Feugiat
+          nibh sed pulvinar proin gravida hendrerit lectus a. Enim facilisis
+          gravida neque convallis a cras. Id leo in vitae turpis massa sed
+          elementum tempus. Varius vel pharetra vel turpis.
+        </div>
+
+        <div className="max-w-[900px] self-center">
+          <img src="./about4.jpg" />
+        </div>
+
+        <div className="text-xl font-bold">
+          Maecenas pharetra convallis posuere morbi leo. Ipsum consequat nisl
+          vel pretium lectus. Risus in hendrerit gravida rutrum quisque non
+          tellus orci ac. Sed risus pretium quam vulputate dignissim suspendisse
+          in. Augue eget arcu dictum varius duis. Diam volutpat commodo sed
+          egestas egestas fringilla phasellus. Nunc non blandit massa enim nec
+          dui nunc mattis enim. Quis blandit turpis cursus in hac habitasse
+          platea. Sapien faucibus et molestie ac feugiat sed lectus vestibulum
+          mattis. Nisl suscipit adipiscing bibendum est ultricies integer quis
+          auctor elit. Enim nec dui nunc mattis enim ut tellus elementum.
+        </div>
+      </div>
+    </div>
+  );
 };
