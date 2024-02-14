918265a95b88dcd7adaa29b84440c642bc142fb7
Francesco <francesco.seddo@gmail.com>
2024-02-14 01:10:01 -0500

    removed logs

--- src/common/convertPrice.ts
+++ src/common/convertPrice.ts
@@ -2,7 +2,6 @@ export const convertPrice = (price: number | undefined) => {
   let priceString = "";
   if (price == null) priceString = "-";
   else {
-    console.log(price);
     const roundedPrice = Math.ceil(price);
     const dollars = roundedPrice.toString().slice(0, -2);
     const cents = roundedPrice.toString().slice(-2);
