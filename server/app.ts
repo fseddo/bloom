3069171eccd64ea9a4d96d3bb14d41c5e89273f8
Francesco <francesco.seddo@gmail.com>
2024-02-14 22:24:10 -0500

    updated port with env variable

--- server/app.ts
+++ server/app.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 export const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //CORS middlware
 app.use(function (req, res, next) {
